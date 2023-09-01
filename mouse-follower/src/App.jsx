d88c475eb02f4f43884e824f042ac376005c8893
Martin Jose Garcia <m.garcia@natureh.com>
2023-09-01 17:41:32 +0200

    final version mouse-follower

---
+++ mouse-follower/src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x:0, y:0})
+
+  useEffect(() => {
+    console.log('efecto ', {enabled})
+    const handleMove = (event) => {
+        const {clientX, clientY} = event
+        console.log( 'handleMove', {clientX, clientY})
+        setPosition({x:clientX , y:clientY})
+    }
+    if (enabled){
+      window.addEventListener('pointermove',handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove',handleMove)
+    } //se ejecuta esto cuando cambia la dependencia
+  },[enabled])
+
+  return (
+    <>
+      <div style={{
+        position:'absolute',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px,${position.y}px)`
+      }}>
+
+      </div>
+      <button onClick={() => setEnabled(!enabled)}>
+          {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </>
+  )
+}
+
+function App() {
+
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  )
+}
+
+export default App
